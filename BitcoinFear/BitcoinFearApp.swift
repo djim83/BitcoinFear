ef278b6f56d072a61bae3a666b193ce1f7e1eaa4
David <davidjim83@icloud.com>
2024-11-27 16:08:22 +0100

    Initial Commit

---
+++ BitcoinFear/BitcoinFearApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinFearApp.swift
+//  BitcoinFear
+//
+//  Created by David Jiménez on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinFearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
